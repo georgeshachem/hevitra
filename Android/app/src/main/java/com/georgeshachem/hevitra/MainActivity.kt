45ec5ed526705a119d50b4f6d09f0131ee37fee9
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2020-02-08 16:05:45 +0200

    check for sensors

--- Android/app/src/main/java/com/georgeshachem/hevitra/MainActivity.kt
+++ Android/app/src/main/java/com/georgeshachem/hevitra/MainActivity.kt
@@ -1,12 +1,85 @@
 package com.georgeshachem.hevitra
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private val wifiManager: WifiManager
+        get() = getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+    private val wifiScanReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false)
+            if (success) {
+                scanSuccess()
+            } else {
+                scanFailure()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        setupPermissions()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intentFilter = IntentFilter()
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
+        this.registerReceiver(wifiScanReceiver, intentFilter)
+
+        val success = wifiManager.startScan()
+        if (!success) {
+            sensor_status_text.text = "Failed to scan for sensors"
+        }
+
     }
+
+
+    private fun scanSuccess() {
+        val results = wifiManager.scanResults
+        for (item in results) {
+            if (item.SSID == "HevitraSensor"){
+                sensor_status_text.text = "A sensor has been found!"
+                return
+            }
+        }
+        sensor_status_text.text = "No sensor found!"
+
+    }
+
+    private fun scanFailure() {
+        sensor_status_text.text = "Failed to get scan results"
+    }
+
+    private val locationRequestCode = 99
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            locationRequestCode)
+    }
+
 }
