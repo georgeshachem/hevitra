e9127337653e5fb2c437bcd8ba1a36663d8960ec
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2020-02-08 18:25:32 +0200

    added button to connect to sensor wifi

--- Android/app/src/main/java/com/georgeshachem/hevitra/MainActivity.kt
+++ Android/app/src/main/java/com/georgeshachem/hevitra/MainActivity.kt
@@ -1,18 +1,20 @@
 package com.georgeshachem.hevitra
 
+import android.Manifest
+import android.R.attr.key
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.wifi.WifiConfiguration
 import android.net.wifi.WifiManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-
-import android.Manifest
-import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,6 +39,21 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //start button
+        btn_connect.setOnClickListener {
+            val wifiConfig = WifiConfiguration()
+            wifiConfig.SSID = java.lang.String.format("\"%s\"", "HevitraSensor")
+            wifiConfig.preSharedKey = String.format("\"%s\"", "HevitraPassword")
+
+            val wifiManager =
+                getSystemService(Context.WIFI_SERVICE) as WifiManager
+            val netId = wifiManager.addNetwork(wifiConfig)
+            wifiManager.disconnect()
+            wifiManager.enableNetwork(netId, true)
+            wifiManager.reconnect()
+        }
+        //end button
 
         val intentFilter = IntentFilter()
         intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
