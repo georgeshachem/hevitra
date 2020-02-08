f4528ae05b258bf15da25cdc6a9adcfca2f31fdb
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2020-02-08 22:35:49 +0200

    esp8266 webserver to receive and connect to wifi network

--- esp8266/esp8266.ino
+++ esp8266/esp8266.ino
@@ -1,4 +1,10 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+IPAddress apIP(192, 168, 105, 1);
+ESP8266WebServer server(80);
+
+void handleConfig();
 
 void setup()
 {
@@ -6,10 +12,14 @@ void setup()
   Serial.println();
 
   Serial.print("Setting soft-AP ... ");
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   boolean result = WiFi.softAP("HevitraSensor", "HevitraPassword");
-  if(result == true)
+  if (result == true)
   {
     Serial.println("Ready");
+    server.on("/config", handleConfig);
+    server.begin();
+    Serial.println("Server listening");
   }
   else
   {
@@ -19,6 +29,33 @@ void setup()
 
 void loop()
 {
-  Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
-  delay(3000);
+  server.handleClient();
+}
+
+void handleConfig() {
+  server.send(200, "text/plain", "200: Trying to connect");
+
+  String ssid = server.arg("ssid");
+  String password = server.arg("password");
+
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.print(ssid); Serial.println(" ...");
+
+  int i = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+    if (i == 20)
+    {
+      Serial.println("Failed to connect");
+      return;
+    }
+  }
+
+  Serial.println('\n');
+  Serial.println("Connection established!");
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());
+  WiFi.softAPdisconnect(true);
 }
